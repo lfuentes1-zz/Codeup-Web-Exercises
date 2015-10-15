96f3abb47bdfce0a5cfb32414c8d63c76875769b
Letty Fuentes <letty@Lettys-MacBook-Air.local>
2015-10-15 11:54:01 -0500

    Codeup Exercises

---
+++ public/js/jquery_exercises.js
@@ -0,0 +1,16 @@
+(function (){
+
+	"use scrict";
+
+
+	$(document).ready(function() {
+        $('.codeup').css('border', '10px solid red');
+        $('#my_list').css('font-size', '40px');
+		$('#my_heading').css('font-size', '40px');            
+		var my_heading2 = $("#my_heading2").html();
+		alert (my_heading2);		
+
+    });
+
+})();
+
