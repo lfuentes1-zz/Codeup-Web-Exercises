0301db92482e8970660dd535aea05cf721a3e6e3
Letty Fuentes <letty@Lettys-MacBook-Air.local>
2015-11-03 15:58:43 -0600

    Improved Readability - Formatting

--- public/php/if.php
+++ public/php/if.php
@@ -20,7 +20,7 @@ if ($a < $b) { //5 < 10
      // output the appropriate result
      echo "$b is greater than or equal to $c\n";
  } else {
-     echo "$b is less than or equal to $c" . PHP_EOL;
+     echo "$b is not greater than or equal to $c" . PHP_EOL;
  }
 
  // if ($b <= $c) { //10 <= '10'
@@ -29,23 +29,13 @@ if ($a < $b) { //5 < 10
  // }
 
  if ($b == $c) { //10 == '10'
-     // output the appropriate result
      echo "$b is equal to $c\n";
- } else {
-    if ($b === $c) { //10 === '10'
-          // output the appropriate result
-          echo "$b is not identical to $c\n";
-     } else {
-         // output the appropriate result
-          if ($b != $c) {
-          echo "$b is not equal to $c\n";
-     } else {
-          // output the appropriate result
-          if ($b !== $c) {
-               echo "$b is not identical to $c\n";
-          }
-          }
-     }
+ } else if ($b === $c) { //10 === '10'
+     echo "$b is not identical to $c\n";
+ } else if ($b != $c) {
+     echo "$b is not equal to $c\n";
+ } else if ($b !== $c) {
+     echo "$b is not identical to $c\n";
  }
  
 //To run go to: /vagrant/sites/codeup.dev/public/php php if.php
