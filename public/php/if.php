ca848ea2f6435339203bb5040acfc123fb73e973
Letty Fuentes <letty@Lettys-MacBook-Air.local>
2015-11-03 15:41:44 -0600

    Refactored with if/else statements

--- public/php/if.php
+++ public/php/if.php
@@ -7,43 +7,46 @@ $c = '10';
 if ($a < $b) { //5 < 10
      // output the appropriate result
      echo "$a is less than $b\n";
+ } else {
+     echo "$b is greater than $a" . PHP_EOL;
  }
 
- if ($b > $a) { //10 > 5
-     // output the appropriate result
-     echo "$b is greater than $a\n";
- }
+ // if ($b > $a) { //10 > 5
+ //     // output the appropriate result
+ //     echo "$b is greater than $a\n";
+ // }
 
  if ($b >= $c) { //10 >= '10'
      // output the appropriate result
      echo "$b is greater than or equal to $c\n";
+ } else {
+     echo "$b is less than or equal to $c" . PHP_EOL;
  }
 
- if ($b <= $c) { //10 <= '10'
-     // output the appropriate result
-     echo "$b is less than or equal to $c\n";
- }
+ // if ($b <= $c) { //10 <= '10'
+ //     // output the appropriate result
+ //     echo "$b is less than or equal to $c\n";
+ // }
 
  if ($b == $c) { //10 == '10'
      // output the appropriate result
      echo "$b is equal to $c\n";
+ } else {
+    if ($b === $c) { //10 === '10'
+          // output the appropriate result
+          echo "$b is not identical to $c\n";
+     } else {
+         // output the appropriate result
+          if ($b != $c) {
+          echo "$b is not equal to $c\n";
+     } else {
+          // output the appropriate result
+          if ($b !== $c) {
+               echo "$b is not identical to $c\n";
+          }
+          }
+     }
  }
-
- if ($b === $c) { //10 === '10'
-     // output the appropriate result
-     echo "$b is not identical to $c\n";
- }
-
- // output the appropriate result
- if ($b != $c) {
-     echo "$b is not equal to $c\n";
- }
-
- // output the appropriate result
- if ($b !== $c) {
-     echo "$b is not identical to $c\n";
- }
-
-
+ 
 //To run go to: /vagrant/sites/codeup.dev/public/php php if.php
  ?>
