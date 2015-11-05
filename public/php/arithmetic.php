8e0181ea89aa0ca3ee9ec5da9c775039be80bad9
Letty Fuentes <letty@Lettys-MacBook-Air.local>
2015-11-05 16:00:04 -0600

    Added Definitions to the Functions

--- public/php/arithmetic.php
+++ public/php/arithmetic.php
@@ -1,23 +1,36 @@
 <?php
 
+$sum = 0;
+$subtraction = 0;
+$multiplication = 0;
+$division = 0;
+ 
 function add($a, $b)
 {
-    echo $a + $b;
+    return $a + $b;
 }
 
 function subtract($a, $b)
 {
-    // Add code here
+	return $a - $b;
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+	return $a * $b;
 }
 
 function divide($a, $b)
 {
-    // Add code here
+	return $a / $b;
 }
 
 // Add code to test your functions here
+$sum = add (10, 5);
+echo "The sum is {$sum}" . PHP_EOL;
+$subtraction = subtract (15, 5);
+echo "The subtraction is {$subtraction}" . PHP_EOL;
+$multiplication = multiply (3, 3);
+echo "The multiplication is {$multiplication}" . PHP_EOL;
+$division = divide (15, 5);
+echo "The division is {$division}" . PHP_EOL;
