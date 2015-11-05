38abe556ce85a31fa20a74963cb067cacf309d29
Letty Fuentes <letty@Lettys-MacBook-Air.local>
2015-11-05 14:06:11 -0600

    Added Examples

--- public/php/foreach_books.php
+++ public/php/foreach_books.php
@@ -23,25 +23,26 @@ $books = array(
     )
 );
 
-foreach ($books as $title => $book) {
+foreach ($books as $title => $novel) {
     //The following foreach loop is not necessary
-    // foreach ($book as $key => $value) {
+    // foreach ($novel as $key => $value) {
     // }
-    if ($book['published'] > 1950) {
+    if ($novel['published'] > 1950) {
             echo "Published before 1950!" . PHP_EOL;
     }
     echo "Book Title: {$title}" . PHP_EOL;
-    echo "Year Published: {$book['published']}" . PHP_EOL;
-    echo "Number of Pages: {$book['pages']}" . PHP_EOL;
-    echo "Author: {$book['author']}" . PHP_EOL . PHP_EOL;;
+    echo "Year Published: {$novel['published']}" . PHP_EOL;
+    echo "Number of Pages: {$novel['pages']}" . PHP_EOL;
+    echo "Author: {$novel['author']}" . PHP_EOL . PHP_EOL;;
 }
 
-
 //Another solution
-foreach ($books as $title => $book) {
-    echo "'{$title}':\n";
-    foreach ($book as $key => $data) {
-        echo "{$key} - {$data}\n";
+//For this problem, these nested loops do not provide much control.  If there was more
+// keys, they would print also even though we wouldn't want them to.  Also, keys print in order.
+foreach ($books as $title => $novel) {
+    echo "{$title} " .PHP_EOL;
+    foreach ($novel as $key => $data) {
+        echo "{$key}: {$data}" . PHP_EOL;
     }
     echo PHP_EOL;
 }
