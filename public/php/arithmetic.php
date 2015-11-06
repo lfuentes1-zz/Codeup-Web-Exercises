2f9413c763e7dc2664ac5580736a0ba5ddbe4ed6
Letty Fuentes <letty@Lettys-MacBook-Air.local>
2015-11-06 12:02:00 -0600

    Refactored code on errorMessages function and added validateNumeric function

--- public/php/arithmetic.php
+++ public/php/arithmetic.php
@@ -1,47 +1,66 @@
 <?php
+define ("NON_NUMERIC", "non numeric");
+define ("DIVIDE_BY_ZERO", "divide by zero");
 
 $sum = 0;
 $subtraction = 0;
 $multiplication = 0;
 $division = 0;
 $input1 = 15;
-$input2 = 5;
+$input2 = 0;
  
-function errorMessages ($a_value, $b_value) {
-	return "ERROR: {a: {$a_value}} and {b: {$b_value}} arguments must be numbers" . PHP_EOL;
+function errorMessages ($a_value, $b_value, $errorType) {
+	switch ($errorType) {
+		case 'non numeric':
+			return "Error! Values given must be numeric!  You gave {a: {$a_value}} and {b: {$b_value}}" . PHP_EOL;
+			break;
+		case 'divide by zero':
+			return "Error! Divison by zero!  You gave {a: {$a_value}} / {b: {$b_value}}" . PHP_EOL;
+			break;
+		default:
+			return "Error not found!";
+			break;
+	}
+}
+
+function validateNumeric ($a_value, $b_value) {
+	if (is_numeric($a_value) && is_numeric($b_value)) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 function add($a, $b)
 {
-	if (is_numeric($a) && is_numeric($b)) {
+	if (validateNumeric($a, $b)) {
 		return $a + $b;	
 	} else {
-		return errorMessages($a, $b);
+		return errorMessages($a, $b, NON_NUMERIC);
 	} 
 }
 
 function subtract($a, $b)
 {
-	if (is_numeric($a) && is_numeric($b)) {
+	if (validateNumeric($a, $b)) {
 		return $a - $b;	
 	} else {
-		return errorMessages($a, $b);
+		return errorMessages($a, $b, NON_NUMERIC);
 	}
 }
 
 function multiply($a, $b)
 {
-	if (is_numeric($a) && is_numeric($b)) {
+	if (validateNumeric($a, $b)) {
 		return $a * $b;	
 	} else {
-		return errorMessages($a, $b);
+		return errorMessages($a, $b, NON_NUMERIC);
 	}
 }
 
 function forMultiply ($a, $b)
 {
-	if (is_numeric($a) && (is_numeric($b)))
-	{
+	if (validateNumeric($a, $b)) {
 		$sum = 0;
 		for ($i = 1; $i <= $b; $i++)
 		{	
@@ -49,30 +68,30 @@ function forMultiply ($a, $b)
 		}
 		return $sum;
 	} else {
-		return errorMessages($a, $b);
+		return errorMessages($a, $b, NON_NUMERIC);
 	}
 	//can also re-use the add function inside the for loop to re-use code
 }
 
 function divide($a, $b)
 {
-	if (is_numeric($a) && is_numeric($b)) {
+	if (validateNumeric($a, $b)) {
 		if ($a / $b == 0) {
-			return "ERROR:  Division by zero." . PHP_EOL;
+			return errorMessages ($a, $b, DIVIDE_BY_ZERO);
 		} else {
 			return $a / $b;		
 		}
 	} else {
-		return errorMessages($a, $b);
+		return errorMessages($a, $b, NON_NUMERIC);
 	} 
 }
 
 function modulus($a, $b)
 {
-	if (is_numeric($a) && is_numeric($b)) {
+	if (validateNumeric($a, $b)) {
 		return $a % $b;	
 	} else {
-		return errorMessages($a, $b);
+		return errorMessages($a, $b, NON_NUMERIC);
 	}
 }
 
