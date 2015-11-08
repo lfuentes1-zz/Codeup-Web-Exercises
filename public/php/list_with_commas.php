08bb04b61c34af11d82839fd75faae40ec484c0e
Letty Fuentes <lfuentes1@github.com>
2015-11-08 16:37:44 -0600

    Re-factored solution into a function.

--- public/php/list_with_commas.php
+++ public/php/list_with_commas.php
@@ -1,12 +1,25 @@
 <?php
 
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+$physicistsArray = [];
+$famousFakePhysicists = '';
 
+// Converts array into list n1, n2, ..., and n3
+function humanizedList($physicistsArray){
+	$famousFakePhysicists = '';
 
+	$lastItem = "and " . array_pop($physicistsArray);
+	array_push($physicistsArray, $lastItem);
+	$famousFakePhysicists = implode(', ', $physicistsArray);
+
+	return $famousFakePhysicists;
+}
 
 //Create the $famousFakePhysicists string that lists the physicists and contains the "and" at the end.
+//First, convert the string into an array, broken at the comman(space) delimiter.
 $physicistsArray = explode(', ', $physicistsString);
-$lastItem = "and " . array_pop($physicistsArray);
-array_push($physicistsArray, $lastItem);
-$famousFakePhysicists = implode(', ', $physicistsArray);
+
+// Humanize that list
+$famousFakePhysicists = humanizedList($physicistsArray);
+
 echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." . PHP_EOL;
