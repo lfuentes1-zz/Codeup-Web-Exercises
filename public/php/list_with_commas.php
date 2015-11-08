0254a27e17756e4ced5e8aeb4cdf999555d32826
Letty Fuentes <lfuentes1@github.com>
2015-11-08 16:47:19 -0600

    Create logic to make alphabetic sorting optional (option passed as an argument to the function)

--- public/php/list_with_commas.php
+++ public/php/list_with_commas.php
@@ -3,11 +3,17 @@
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 $physicistsArray = [];
 $famousFakePhysicists = '';
+$sorting = true;
 
 // Converts array into list n1, n2, ..., and n3
-function humanizedList($physicistsArray){
+function humanizedList($physicistsArray, $sorting){
 	$famousFakePhysicists = '';
 
+	//Update your code to list the physicists by first name, in alphabetical order.
+	if ($sorting == true){
+		sort ($physicistsArray);	
+	}
+	
 	$lastItem = "and " . array_pop($physicistsArray);
 	array_push($physicistsArray, $lastItem);
 	$famousFakePhysicists = implode(', ', $physicistsArray);
@@ -18,13 +24,13 @@ function humanizedList($physicistsArray){
 //Create the $famousFakePhysicists string that lists the physicists and contains the "and" at the end.
 //First, convert the string into an array, broken at the comman(space) delimiter.
 $physicistsArray = explode(', ', $physicistsString);
-//Update your code to list the physicists by first name, in alphabetical order.
-sort ($physicistsArray);
 
-// Humanize that list
-$famousFakePhysicists = humanizedList($physicistsArray);
+//Humanize that list
+//The second argument to make alphabetical sorting optional. TRUE = SORT and FALSE = DON'T SORT
+$famousFakePhysicists = humanizedList($physicistsArray, $sorting);
 
 echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." . PHP_EOL;
+
 
 
 
