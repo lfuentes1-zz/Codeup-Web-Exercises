f0a1ebb42746920383442e31b08ec9300799240a
Letty Fuentes <lfuentes1@github.com>
2015-11-09 15:57:13 -0600

    Finalized the merge_arrays file

--- public/php/merge-arrays.php
+++ public/php/merge-arrays.php
@@ -6,7 +6,7 @@ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 $merged_array = [];
 
-function mergeArrays ($array_name1, $array_name2) {
+function combineArrays ($array_name1, $array_name2) {
 	$merged_array = [];
 	foreach ($array_name1 as $array1_data) {
 		foreach ($array_name2 as $array2_data) {
@@ -21,7 +21,7 @@ function mergeArrays ($array_name1, $array_name2) {
 	return $merged_array;
 }
 
-$merged_array = mergeArrays($names, $compare);
+$merged_array = combineArrays($names, $compare);
 print_r ($merged_array);
 
 //Another more cumbersome solution
@@ -31,3 +31,14 @@ print_r ($merged_array);
 // 	array_push($merged_array, $array1_data);//
 // 	array_push($merged_array, $array2_data);
 // }
+
+//Another solution
+// $combined_array = [];
+// foreach ($array1 as $index => $name) {
+// 	if (array_search($name, $array2) ! == false) { 
+// 		array_push($combined_array, $name);
+// 	} else {
+// 		array_push($combined_array, $name, $array2[$index]);
+// 	}
+// }
+// return $combined_array;
