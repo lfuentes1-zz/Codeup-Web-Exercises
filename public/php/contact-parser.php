3cf426a456fc60c7d6cc80fca16ff262649ff96b
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-10 20:42:12 -0600

    Inserted different solutions - denoted by comments

--- public/php/contact-parser.php
+++ public/php/contact-parser.php
@@ -13,17 +13,27 @@ function parseContacts($filename)
     $allContacts = array();
 
     $handle = fopen($filename, 'r');
-	$fileContents = fread($handle, filesize($filename));
-	$fileContents = trim($fileContents);
-
-	$contentsArray = explode("\n", $fileContents);
+	$fileContentsString = fread($handle, filesize($filename));
+	$contentsArray = explode("\n", trim($fileContentsString));
 
 	foreach ($contentsArray as $person) {
 		$peopleArray = explode('|', $person);
+		// array_push ($allContacts, )
 		$allContacts[] = array(
 			'name' => $peopleArray[0],
 			'number' => formatPhoneNumber($peopleArray[1])
 		);
+		//Option 2
+		//$anotherArray = ["name" => $peopleArray[0],
+		//				  "number" => formatPhoneNumber($peopleArray[1])
+		//				 ];
+		//$allContacts[] = $anotherArray; 
+		//The previous line can be deleted as long as we add [] after $anotherArray on line 26
+
+		//Option 3
+		//$allContacts = [];
+		//$allContacts['name'] = $peopleArray[0];
+		//$allContacts[number'] = $formatPhoneNumber($peopleArray[1])
 	}	
 	
 	fclose($handle);
