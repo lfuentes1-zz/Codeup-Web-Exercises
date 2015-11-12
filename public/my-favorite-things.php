8b240f8e60a77976c182156a7fb28fbbcf43bd90
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-12 15:46:28 -0600

    Template code for my-favorite-things (HTML/PHP).  Uses foreach loop inside HTML code.

---
+++ public/my-favorite-things.php
@@ -0,0 +1,25 @@
+<?php
+
+$myFavoriteThings = array ('1thing', '2thing', '3thing', '4thing', '5thing');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My favorite things using inline PHP</title>
+    <link rel="stylesheet" href="/css/my-favorite-things.css">
+</head>
+<body>
+    <h1>My Favorite Things</h1>
+    	<table id="style-table">
+		    	<?php foreach ($myFavoriteThings as $thing)
+		    	{ ?>
+		    	<tr>
+		    		<td> <?php echo $thing; ?> <br> 
+		    		</td>
+		    	<tr>
+		    	<?php } ?>
+		</table>
+</body>
+</html>
