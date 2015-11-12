46a7630942ac17327aea71889b193b44487803d7
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-12 15:47:45 -0600

    Template code for server-name-generator.php.  Uses a function declared in PHP tags.  HTML code uses the results of the PHP function.

---
+++ public/server-name-generator.php
@@ -0,0 +1,59 @@
+<?php
+
+//Adjectives array
+$adjectivesArray = array('bright', 
+			   			'clean',
+			   			'shiny',
+			   			'careful', 
+			   			'outstanding',
+			   			'talented',
+			   			'helpful',
+			   			'poised',
+			   			'distinct',
+			   			'clever');
+//Nouns array
+$nounsArray = array('eyelash',
+					'skin',
+					'child',
+					'book',
+					'adult',
+					'flowers',
+					'nurse',
+					'room',
+					'pajamas',
+					'waterfall');
+
+
+function randomServerNameGenerator($localAdjectivesArray, $localNounsArray){
+	//Option #1
+	// $max = sizeof ($localAdjectivesArray);
+	// $randomNumber = rand(0, $max - 1);
+	// $adjective = $localAdjectivesArray[$randomNumber];
+	// $randomNumber = rand(0, $max -1);
+	// $noun = $localNounsArray[$randomNumber];
+	// $localServerName = $adjective . ' ' . $noun;
+
+	$adjective = array_rand($localAdjectivesArray);
+	$noun = array_rand($localNounsArray);
+	$localServerName = $localAdjectivesArray[$adjective] . '-' . 
+					   $localNounsArray[$noun];
+	return $localServerName;
+}
+$serverName = randomServerNameGenerator ($adjectivesArray, $nounsArray);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Server name generator using inline PHP</title>
+    <link rel="stylesheet" href="/css/server-name-generator.css">
+</head>
+<body>
+    <h1>Server Name Generator</h1>
+    <h3>Your server name is: 
+    	<span>
+    		<?php echo $serverName; ?>
+    	</span>
+    </h3>
+</body>
+</html>
