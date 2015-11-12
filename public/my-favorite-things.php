673a6322eac7ca91fddb7215ae6affb1a4acd3f0
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-12 15:53:26 -0600

    Replaced long syntax with short syntax in the HTML code

--- public/my-favorite-things.php
+++ public/my-favorite-things.php
@@ -13,13 +13,12 @@ $myFavoriteThings = array ('1thing', '2thing', '3thing', '4thing', '5thing');
 <body>
     <h1>My Favorite Things</h1>
     	<table id="style-table">
-		    	<?php foreach ($myFavoriteThings as $thing)
-		    	{ ?>
+		    	<?php foreach ($myFavoriteThings as $thing): ?>
 		    	<tr>
-		    		<td> <?php echo $thing; ?> <br> 
+		    		<td> <?= $thing; ?> <br> 
 		    		</td>
 		    	<tr>
-		    	<?php } ?>
+		    	<?php endforeach; ?>
 		</table>
 </body>
 </html>
