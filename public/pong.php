465b37cfd760b37e5016f56eee511c440ca965f4
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-13 14:48:09 -0600

    Template code (HTML/PHP).  Toggles back and forth to ping.php depending on the link that is clicked.

---
+++ public/pong.php
@@ -0,0 +1,45 @@
+<?php
+function increaseCounter ($counterToUp){
+	$counterToUp++;
+	return $counterToUp;
+}
+
+function gameOver(){
+	return 'Game Over';
+}
+
+function pageController() {
+	$click = isset($_GET['click']) ? $_GET['click'] : 0;
+
+	$hit = increaseCounter($click);
+	$miss = gameOver();
+	
+	return array (
+		'hit'   => $hit,
+		'miss' => $miss,
+		'click' => $click
+		);
+}
+extract(pageController());
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hit or Miss Game</title>
+    <!-- <link rel="stylesheet" href="/css/server-name-generator.css"> -->
+</head>
+<body>
+    <h2>Your counter value:
+    	<span>
+    		<?= $click; ?>
+    	</span>
+    </h2>
+    <?php if ($click !== "Game Over"): ?>
+    	<a href="ping.php?click=<?= $hit; ?>">Hit</a> 	
+    	<a href="pong.php?click=<?= $miss; ?>">Miss</a> 	
+	<?php else: ?>
+	    <a href="pong.php">New Game</a> 		
+	<?php endif; ?>
+</body>
+</html>
