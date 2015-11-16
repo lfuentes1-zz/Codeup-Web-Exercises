1b7e3c8313785bf6628711ddbbfeebf8cde3065b
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-16 11:09:58 -0600

    Changed the solution to fix bugs

--- public/login.php
+++ public/login.php
@@ -2,15 +2,23 @@
 	
 	var_dump ($_POST);
 
-	$username = isset($_POST['username']) ? $_POST['username'] : '';
-	$password = isset($_POST['password']) ? $_POST['password'] : ''; 
+	$message = "";
+	$username = "";
 
-	if (($username == "guest") && ($password == "password"))
-	{ 
-		header("location: authorized.php");
-	} else {
-		if (isset($_POST['username']) && isset($_POST['password'])) {
-			header("location: failed.php");
+	if (isset($_POST['username']) && isset($_POST['password'])) {
+		// $username = isset($_POST['username']) ? $_POST['username'] : '';
+		// $password = isset($_POST['password']) ? $_POST['password'] : ''; 
+		//we already know that $username and $password are set so rewrite the previous two lines as follows
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+	
+		if (($username == "guest") && ($password == "password"))
+		{ 
+			header("location: authorized.php");
+			//after a header redirect, php keeps running remaining code, die() prevents that from happening.
+			die ();
+		} else {
+			$message = "Input a valid username and password!";
 		}
 	}
  ?>
@@ -33,11 +41,11 @@
 				<label for="username" form="verify">Username</label>
 				<!-- type=specifies the type<input>element to display
 				name=specifies the name of the input element -->
-				<input type="text" name="username" id="username"><br>
+				<input type="text" name="username" id="username" value="<?= $username ?>"><br>
 			</p>
 			<p>
 				<label for="password" form="verify">Password</label>
-				<input type="text" name="password" id="password"><br>
+				<input type="password" name="password" id="password"><br>
 			</p>
 			<!--
 			type=specifies the type of button
@@ -45,5 +53,6 @@
 			value=specifies the initial value for the button -->
 			<button type="submit" name="send" id="send" value="submit">Submit</button>
 		</form>
+		<h2><?= $message ?></h2>
 	</body>
 </html>
