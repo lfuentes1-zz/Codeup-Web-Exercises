b7d90affea099971f890799a19d6ba6bd227034b
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-17 09:30:17 -0600

    Moved the projects to their own repository

--- public/WebApp_iPad/Exercise Files/3 - Code Snippets/K-snippets-check-standalone.js
+++
@@ -1,8 +0,0 @@
-/* JavaScript snippet: Written by Chris Converse for lynda.com */
-
-function checkiPadStandAlone(){
-	if(window.navigator.standalone == false) {
-		$('.page').css('display','none');
-		$('body').css('background-image','url(assets/images/template/background_content_home.jpg?v=1)').append('<img  src="assets/images/template/add_to_homescreen.png?v=1"/>');
-	}
-}
