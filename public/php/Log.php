60889d3b4ab1bce4c7d9c8bf3dd56ea025bfe308
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-17 13:06:47 -0600

    Class Log definition to be used in log-test.php

---
+++ public/php/Log.php
@@ -0,0 +1,22 @@
+<?php
+
+class Log {
+	public $filename;
+
+	public function logMessage($logLevel, $message)
+    {
+    	$handle = fopen($this->filename, 'a');
+    	$data = date("Y-m-d") . ' ' . date("H:i:s") . ' [' . $logLevel . '] ' . $message;
+    	fwrite($handle, $data . PHP_EOL);
+		fclose($handle);
+    }	
+
+    public function info($message){
+		$this->logMessage ("INFO", $message);
+		$this->logMessage ("CRITICAL", $message);    
+	}
+
+    public function error($message){
+		$this->logMessage ("ERROR", $message);
+    }
+}
