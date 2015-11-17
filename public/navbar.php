e81aced0556259aae86bc7af589ec852b562c517
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-16 21:10:20 -0600

    This navbar.php is a required file for index_site.php

---
+++ public/navbar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Navbar.php=>index_site.php</title>
+</head>
+<body>
+	<nav>
+  		<a href="calculator.html">Calculator</a> |
+  		<a href="ping.php">Ping/Pong Game</a> |
+  		<a href="weather_map.html">Weather Application</a> |
+  		<a href="server-name-generator.php">Server Name Generator</a>
+	</nav>
+</body>
+</html>
