8c6d5a1b747b999258a6a13ea8345a001ff8a634
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-16 21:08:32 -0600

    This footer file is a required file of index_site.php

---
+++ public/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Footer.php=>index_site.php</title>
+</head>
+<body>
+	<footer>
+  		<p>Posted by: Letty Fuentes</p>
+  		<p>Contact information: <a href="mailto:lettyfuentes2013@gmail.com">
+  		<p>lettyfuentes2013@gmail.com</a>.</p>
+	</footer>
+</body>
+</html>
