5fbd090293fea12eba0e12816b80f7f533f94b2e
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-16 21:09:46 -0600

    This header.php is a required file of index_site.php

---
+++ public/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Header.php=>index_site.php</title>
+</head>
+<body>
+	<header>
+		<h1>Welcome to Letty's Project Site!</h1>
+	</header>
+</body>
+</html>
