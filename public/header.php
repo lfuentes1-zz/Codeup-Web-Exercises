5b1bf9ebe900358e5e9445af5c428a2bb70ba493
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-17 12:10:19 -0600

    Removed unnecessary code since it was included in the index_site.php file already.

--- public/header.php
+++ public/header.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Header.php=>index_site.php</title>
-</head>
-<body>
-	<header>
-		<h1>Welcome to Letty's Project Site!</h1>
-	</header>
-</body>
-</html>
+<header>
+	<h1>Welcome to Letty's Project Site!</h1>
+</header>
