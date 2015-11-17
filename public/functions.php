5cf01874ce33275c9bf39476a8b334356aff25c7
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-16 20:32:03 -0600

    Functions file contains three functions to be used as a required file in ping.php and pong.php

---
+++ public/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+// inputHas($key): returns true or false based on whether the key is available.
+function inputHas ($key) {
+	return (isset($_REQUEST[$key])) ? TRUE : FALSE;
+}
+
+// inputGet($key): returns the value specified by the key, or null if the key is not set.
+function inputGet($key) {
+	return (isset($_REQUEST[$key])) ? $_REQUEST[$key] : NULL;
+}
+
+// escape($input): returns the input as a safely escaped string.
+function escape($input) {
+	return htmlspecialchars(strip_tags($input));
+}
