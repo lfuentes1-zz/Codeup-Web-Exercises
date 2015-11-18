b269350788e2a9869bed61afb083c59c669c6daf
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-18 09:38:36 -0600

    Changed the parameter of object Log() as appropriate

--- public/php/log-test.php
+++ public/php/log-test.php
@@ -4,7 +4,7 @@ require_once 'Log.php';
 date_default_timezone_set("America/Chicago");
 
 // Create an instance of the Log class
-$logger = new Log('../data/cli-');
+$logger = new Log('cli');
 
 $logger->info("This is an info message");
 $logger->error("This is an error message.");
