7014ab0350ce841d8661652cba231816bd18c9af
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-18 17:18:20 -0600

    Fix bugs and remove unnecessary code

--- lib/Auth.php
+++ lib/Auth.php
@@ -1,19 +1,16 @@
 <?php
 
-require_once 'Log.php';   //check the path
-require_once 'Input.php';  //check the path
-
-// Class definition will be used in login.php, authorized.php, and logout.php
+require_once '../public/php/Log.php';
 
 class Auth {
 
 	public static $password = '$2y$10$SLjwBwdOVvnMgWxvTI4Gb.YVcmDlPTpQystHMO2Kfyi/DS8rgA0Fm';
 
-	public static attempt ($username, $password){
-		$logger = new Log('log');
-		$username = Input::get($_SESSION['username']); 
+	public static function attempt ($username, $password){
+		$logger = new Log('../public/data/log');
 
-		if ($username === 'guest') && (password_verify($password, self::password) {
+		if (($username === 'guest') && (password_verify($password, self::$password)))
+		{
 			$_SESSION['LOGGED_IN_USER'] = TRUE;
 			$_SESSION['username'] = $username;
 			$logger->info("User {$username} logged in.");
@@ -24,19 +21,17 @@ class Auth {
 		}
 	}
 
-	public static check (){
-		return ($_SESSION['LOGGED_IN_USER']) ? return TRUE : return FALSE;
+	public static function check (){
+		return ($_SESSION['LOGGED_IN_USER']) ? TRUE : FALSE; //should I have isset in the condition
 	}
 
-	public static user (){
-	// Auth::user() will return the username of the currently logged in user.
-		return Input::get($_SESSION['username']);
+	public static function user (){
+		return ($_SESSION['username']);
 	}
 
-	public static logout(){
+	public static function logout(){
 		if (isset($_SESSION['LOGGED_IN_USER']))
 		{
-			// $_SESSION['LOGGED_IN_USER'] = FALSE;
 			unset($_SESSION['LOGGED_IN_USER']);
 		}
 	}
