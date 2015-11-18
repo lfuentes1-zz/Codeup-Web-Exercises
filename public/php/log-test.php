56382ec5b436b023dd4ba37dd5955ed8135b2c64
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-17 18:42:46 -0600

    Removed the $filename definition.  Added the definition to the constructor method for the class.

--- public/php/log-test.php
+++ public/php/log-test.php
@@ -6,8 +6,5 @@ date_default_timezone_set("America/Chicago");
 // Create an instance of the Log class
 $logger = new Log();
 
-// Set the $filename property in the class; use the format log-YYYY-MM-DD.log.
-$logger->filename = '../data/log-' . date("Y-m-d") . '.log';
-
 $logger->info("This is an info message");
 $logger->error("This is an error message.");
