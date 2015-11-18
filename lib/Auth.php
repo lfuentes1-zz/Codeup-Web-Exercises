df8ec4ecc7dbd25878110ccb3590319511bca764
Letty Fuentes <letty@MacBook-Pro.local>
2015-11-18 14:23:46 -0600

    Added functionality to the user method

--- lib/Auth.php
+++ lib/Auth.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once 'Log.php'; 
+require_once 'Log.php';   //check the path
+require_once 'Input.php';  //check the path
 
 // Class definition will be used in login.php, authorized.php, and logout.php
 
@@ -10,9 +11,11 @@ class Auth {
 
 	public static attempt ($username, $password){
 		$logger = new Log('log');
+		$username = Input::escape($_POST['username']); //Ben thinks this should be input get
 
 		if ($username === 'guest') && (password_verify($password, self::password) {
 			$_SESSION['LOGGED_IN_USER'] = TRUE;
+
 			$logger->info("User {$username} logged in.");
 		} else {
 			$logger->info("User {$username} failed to log in.");
@@ -25,8 +28,7 @@ class Auth {
 
 	public static user (){
 	// Auth::user() will return the username of the currently logged in user.
-
-
+		return Input::get($_SESSION['username']);
 	}
 
 	public static logout(){
