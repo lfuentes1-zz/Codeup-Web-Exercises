372aff25880b62d48e6e7e80520cd2ddb1e20c05
Letty Fuentes <lettyfuentes2013@gmail.com>
2015-12-04 11:30:52 -0600

    Modified procedural code to take advantage of the newly updated classes

--- public/shapes_test.php
+++ public/shapes_test.php
@@ -5,12 +5,15 @@ require_once '../lib/Square.php';
 
 $rectangle = new Rectangle(4, 3);
 echo "The area of rectangle one is: {$rectangle->area()}" . PHP_EOL;
+echo "The perimeter of rectangle one is: {$rectangle->perimeter()}" . PHP_EOL;
 
 $rectangle2 = new Rectangle(5, 4);
 echo "The area of rectangle two is: {$rectangle2->area()}" . PHP_EOL;
+echo "The perimeter of rectangle two is: {$rectangle2->perimeter()}" . PHP_EOL;
 
 $rectangle3 = new Rectangle(10, 10);
 echo "The area of rectangle three is: {$rectangle3->area()}" . PHP_EOL;
+echo "The perimeter of rectangle three is: {$rectangle3->perimeter()}" . PHP_EOL;
 
 $square = new Square(4, 4);
 echo "The area of square one is: {$square->area()}" . PHP_EOL;
@@ -23,3 +26,4 @@ echo "The perimeter of square two is: {$square2->perimeter()}" . PHP_EOL;
 $square3 = new Square(6, 6);
 echo "The area of square three is: {$square3->area()}" . PHP_EOL;
 echo "The perimeter of square three is: {$square3->perimeter()}" . PHP_EOL;
+
