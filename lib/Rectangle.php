6b42c9d48e882c5eccb2659f6ad638e80a93d15f
Letty Fuentes <lettyfuentes2013@gmail.com>
2015-12-04 11:30:05 -0600

    Updated perimeter() and area() methods

--- lib/Rectangle.php
+++ lib/Rectangle.php
@@ -11,12 +11,17 @@ class Rectangle {
         $this->width = $width;
     }
 
+    //this is the base method
     public function area ()
     {
     	return $this->height * $this->width;
     }
+
+    //this is the base method
+    public function perimeter ()
+    {
+    	return (($this->width * 2) + ($this->height * 2));
+    }
 }
-
-
 // A static property is a variable that belongs to the class itself, and not to any particular instance of the class. 
 // If a property is declared as static, we should no longer read or write it using an instance of the class. 
