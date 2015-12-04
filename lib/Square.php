b5c62ea30d831145cfeb0e2de2c27fec6b7ded16
Letty Fuentes <lettyfuentes2013@gmail.com>
2015-12-04 10:36:13 -0600

    Overriding the Rectangle constructor

--- lib/Square.php
+++ lib/Square.php
@@ -3,6 +3,12 @@ require_once 'Rectangle.php';
 
 class Square extends Rectangle
 {
+    public function __construct($side)
+    {
+        $this->height = $side;
+        $this->width = $side;
+    }
+
     // In the new Square class, create a method called perimeter that returns the perimeter of the square.
     public function perimeter ()
     {
@@ -10,3 +16,4 @@ class Square extends Rectangle
     }
 
 }
+
