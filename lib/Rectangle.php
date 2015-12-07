d415d0a11be0319118683ee13917e0f9375eeffc
Letty Fuentes <lettyfuentes2013@gmail.com>
2015-12-07 10:28:12 -0600

    Added getter and setter functions

--- lib/Rectangle.php
+++ lib/Rectangle.php
@@ -5,12 +5,25 @@ class Rectangle {
 	private $height;
 	private $width;
 
-	public function __construct($height, $width)
+    public function __construct($height, $width)
     {
-        $this->height = $height;
-        $this->width = $width;
+        //the below line could also be used instead of the calling the setter functions since this class itself can access
+        // its own properties
+        // $this->height = $height;
+        $this->setHeight($height);
+        $this->setWidth($width);
     }
 
+	protected function setHeight ($height)
+    {
+        $this->height = $height;
+    }
+
+    protected function setWidth ($width)
+    {
+        $this->width = $width;
+    }
+    
     //this is the base method
     public function area ()
     {
@@ -21,6 +34,16 @@ class Rectangle {
     public function perimeter ()
     {
     	return (($this->width * 2) + ($this->height * 2));
+    }
+
+    protected function getHeight ()
+    {
+        return $this->height;
+    }
+
+    protected function getWidth ()
+    {
+        return $this->width;
     }
 }
 // A static property is a variable that belongs to the class itself, and not to any particular instance of the class. 
